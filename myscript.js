ecab4b0184bcd388734ea3d2eb1c85d007d21cb6
Shensta <tomshen@tom.local>
2017-07-26 01:28:10 -0400

    jquery commit

--- myscript.js
+++ myscript.js
@@ -1,15 +1,1 @@
-var shouldChange = true;
-function checkDOMChange()
-{
-    if (document.getElementsByClassName("Am Al editable LW-avf").length > 0) {
-        if(shouldChange == true) {
-            document.getElementsByClassName("Am Al editable LW-avf")[0].innerHTML = "helloworld";
-            shouldChange = false;
-        }
 
-    }
-    console.log(shouldChange);
-    setTimeout( checkDOMChange, 100 );
-}
-
-checkDOMChange();
