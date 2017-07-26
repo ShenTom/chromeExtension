f10ae221c227d4382114c1ab2402d2eca564ecc0
Edward Feng <edward0414@gmail.com>
2017-07-26 01:30:40 -0400

    empty

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
+// background.js
 
-//document.getElementById("myBtn").addEventListener("click", insert);
-
-function sendInsertRequest() {
+function sendInsertRequest(){
+// Called when the user clicks on the browser action.
+//chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-    console.log("sent");
   });
-    
+//});
 }
--- myscript.js
+++ myscript.js
@@ -1,45 +1,51 @@
-var shouldChange = true; //change this to onclick event?
+
 var retreivedData = "Hello Tom!/n/nI am Edward./n/nHere is my secret./nI think I am very handsome."; //change this to the retrieved data
 
 function insertEmailContent(content) {
     if (document.getElementsByClassName("Am Al editable LW-avf").length > 0) {
-        if(shouldChange == true) {
-            //chop the message into different divisions based on "/n"
-            //for loop to add div tag to message
-            var isFirst = true;
-            var res = "";
-            var numDiv =0;
-            for (i = 0; i < content.length; i++) {
-                if (content[i] == "/" && content[i+1] == "n") {
-                    if (isFirst == true) {
-                        res = content.slice(0, i) + "<div>" + content.slice(i+2,);
-                        isFirst = false;
-                        numDiv += 3;
 
+        //chop the message into different divisions based on "/n"
+        //for loop to add div tag to message
+        var isFirst = true;
+        var res = "";
+        var numDiv =0;
+        for (i = 0; i < content.length; i++) {
+            if (content[i] == "/" && content[i+1] == "n") {
+                if (isFirst == true) {
+                    res = content.slice(0, i) + "<div>" + content.slice(i+2,);
+                    isFirst = false;
+                    numDiv += 3;
+
+                } else {
+
+                    if (content[i-2] == "/" && content[i-1] == "n"){
+                        res = res.slice(0, i+numDiv) + "<br>" + "</div>" + "<div>" + content.slice(i+2,);
+                        numDiv += 13;
                     } else {
-                        
-                        if (content[i-2] == "/" && content[i-1] == "n"){
-                            res = res.slice(0, i+numDiv) + "<br>" + "</div>" + "<div>" + content.slice(i+2,);
-                            numDiv += 13;
-                        } else {
-                            res = res.slice(0, i+numDiv) + "</div>" + "<div>" + content.slice(i+2,);
-                            numDiv += 9;
-                        }
+                        res = res.slice(0, i+numDiv) + "</div>" + "<div>" + content.slice(i+2,);
+                        numDiv += 9;
                     }
                 }
-            
-                if (i == content.length -1 && isFirst!= true) {
-                    res += "</div>";
-                }
             }
-            console.log(res);
-            document.getElementsByClassName("Am Al editable LW-avf")[0].innerHTML = res;
-            shouldChange = false;
-        }
 
+            if (i == content.length -1 && isFirst!= true) {
+                res += "</div>";
+            }
+        }
+        console.log(res);
+        document.getElementsByClassName("Am Al editable LW-avf")[0].innerHTML = res;
+        
     }
-    console.log(shouldChange);
-    setTimeout(insertEmailContent, 100, content);
 }
 
-insertEmailContent(retreivedData);
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if(request.message == "clicked_browser_action" ) {
+      console.log("received request!");
+      insertEmailContent(retreivedData);
+    }
+  }
+);
+
+
