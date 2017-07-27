0112cd9a9bb6f66b2b6c66822d5a2e8f0292dff5
Edward Feng <edward0414@gmail.com>
2017-07-26 23:00:16 -0400

    Update popup interface

--- popup.js
+++ popup.js
@@ -19,15 +19,27 @@ $(document).ready(function(){
     // gets all emails in database
 
     chrome.storage.local.get("emails",function(result){
-        var r =  result["emails"];
+        var r =  JSON.parse(result["emails"]);
+        
+        for(i in r){
+            console.log(r[i]);
+        }
         // you have to do everything with the emails inside this call back function
+        //where i am gonna dynamically build the html
+        //for loop, check each item's status before add
     });
 })
 
 
+//function that builds the html box
+function buildBox(name, content) {
+    
+}
+
 function sendInsertRequest() {
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    //send data to content script
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "insert"});
     console.log("sent insert");
